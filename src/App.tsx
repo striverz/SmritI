b76b5014e2e4d6c07e0ec35bfed3980005894245
striverz <striverzarc@gmail.com>
2025-11-20 17:24:14 +0530

    Initial commit #project smriti

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center text-blue-400 p-8 text-4xl hover:text-6xl transition-all duration-500 ease-in-out cursor-pointer">
+      Project S M R I T I
+    </div>
+  );
+};
+
+export default App;
